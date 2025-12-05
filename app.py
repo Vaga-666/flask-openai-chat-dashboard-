c8f2053aa2527bccfaf6db92f94b75d0670dcc7b
Vaga Oganyan <oganyan-mustang@mail.ru>
2025-12-05 22:48:22 +0400

    Refactor main block to create database context

--- app.py
+++ app.py
@@ -173,9 +173,7 @@ def dashboard():
 
 
 # Запуск
-if __name__ == '__main__':
-    if os.path.exists("chat.db"):
-        os.remove("chat.db")
+if __name__ == '__main__':        
     with app.app_context():
         db.create_all()
     app.run(debug=True, host="127.0.0.1", port=5000)
